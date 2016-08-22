cb0a38a9ce62cfe8644dd66d9464cba0c41d2f2c
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-08-22 12:46:33 +0300

    updated views using mixins

--- matches/views.py
+++ matches/views.py
@@ -1,44 +1,32 @@
 from matches.models import Match
 from matches.serializers import MatchSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
-class MatchList(APIView):
-    def get(self, request, format=None):
-        matches = Match.objects.all()
-        serializer = MatchSerializer(matches, many=True)
-        return Response(serializer.data)
+class MatchList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
 
-    def post(self, request, format=None):
-        serializer = MatchSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class MatchDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Match.objects.get(pk=pk)
-        except Match.DoesNotExist:
-            raise Http404
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        match = self.get_object(pk)
-        serializer = MatchSerializer(match)
-        return Response(serializer.data)
+class MatchDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
 
-    def put(self, request, pk, format=None):
-        match = self.get_object(pk)
-        serializer = MatchSerializer(match, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        match = self.get_object(pk)
-        match.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
