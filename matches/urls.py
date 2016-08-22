92febb2f211641a27ed7699035a966aaddeddd23
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-08-22 20:21:02 +0200

    using routers

--- matches/urls.py
+++ matches/urls.py
@@ -1,22 +1,12 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from matches import views
 
-match_list = views.MatchViewSet.as_view({
-	'get': 'list',
-	'post': 'create',
-})
-match_detail = views.MatchViewSet.as_view({
-	'get': 'retrieve',
-	'put': 'update',
-	'patch': 'partial_update',
-	'delete': 'destroy',
-})
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'matches', views.MatchViewSet)
 
 urlpatterns = [
-	url(r'^$', views.api_root),
-    url(r'^matches/$', match_list, name='match-list'),
-    url(r'^matches/(?P<pk>[0-9]+)/$', match_detail, name='match-detail'),
+	url(r'^', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- matches/views.py
+++ matches/views.py
@@ -1,15 +1,8 @@
-from rest_framework.decorators import api_view, detail_route
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
+# from rest_framework.decorators import detail_route
+# from rest_framework.response import Response
 from rest_framework import viewsets
 from matches.models import Match
 from matches.serializers import MatchSerializer
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'matches': reverse('match-list', request=request, format=format),
-    })
 
 class MatchViewSet(viewsets.ModelViewSet):
     queryset = Match.objects.all()
