38f81b0de7f7a3689eb3ab531aeb836068c1af23
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-08-22 12:21:43 +0300

    changed to @api_view

--- matches/urls.py
+++ matches/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from matches import views
 
 urlpatterns = [
     url(r'^matches/$', views.match_list),
     url(r'^matches/(?P<pk>[0-9]+)/$', views.match_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- matches/views.py
+++ matches/views.py
@@ -1,53 +1,41 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from matches.models import Match
 from matches.serializers import MatchSerializer
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-@csrf_exempt
-def match_list(request):
+@api_view(['GET', 'POST'])
+def match_list(request, format=None):
     if request.method == 'GET':
         matches = Match.objects.all()
         serializer = MatchSerializer(matches, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MatchSerializer(data=data)
+        serializer = MatchSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def match_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def match_detail(request, pk, format=None):
     try:
         match = Match.objects.get(pk=pk)
     except Match.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = MatchSerializer(match)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = MatchSerializer(match, data=data)
+        serializer = MatchSerializer(match, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         match.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
