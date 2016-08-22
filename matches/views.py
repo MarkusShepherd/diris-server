d9c96e8ad9e0479212c68c1c86f4d7bf6d700342
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-08-22 12:47:45 +0300

    updated views using generics

--- matches/views.py
+++ matches/views.py
@@ -1,32 +1,11 @@
 from matches.models import Match
 from matches.serializers import MatchSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class MatchList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class MatchList(generics.ListCreateAPIView):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class MatchDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class MatchDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
