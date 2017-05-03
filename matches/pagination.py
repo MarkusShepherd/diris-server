8901b0448a2353142094348d6691daa568ed25fb
Markus Shepherd <markus@idtmessaging.com>
2017-05-03 11:06:54 +0300

    added a few more pagination options

--- matches/pagination.py
+++ matches/pagination.py
@@ -9,3 +9,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class GaePageNumberPagination(PageNumberPagination):
 	django_paginator_class = DjangaePaginator
+	page_size_query_param = 'page_size'
+	max_page_size = 50
